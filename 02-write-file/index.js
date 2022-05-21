f4178085167301c550a38480f91acb25a19d75d2
Artem Korotkov <avkor66@gmail.com>
2022-05-21 23:49:04 +0500

    fix: added welcome message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,22 +2,17 @@
 const path = require('path');
 const { stdin } = require('process');
 
-
 let writeStream = fs.createWriteStream(path.resolve(__dirname, 'random.txt'));
-console.log('Введите текст для записи в файл "random.txt"');
+console.log('Привет, введите текст для записи в файл "random.txt"');
 stdin.on('data', chunk => {
-  let temp = chunk.toString();
-  temp = temp.substring(0, temp.length - 1);
+  let temp = chunk.toString().substring(0, chunk.toString().length - 1);
+  temp = temp;
   if (temp === 'exit') { 
-    console.log('Запись прервана, данные в файле: "random.txt"');
+    console.log('Спасибо, запись прервана, данные в файле: "random.txt"');
     process.exit();
-  } else {
-    writeStream.write(temp + '\n', () => {
-      console.log(`Записано: "${temp}"`);
-    })
-  }
+  } else writeStream.write(temp + '\n')
 })
 process.on('SIGINT', function() {
-  console.log('\nЗапись прервана, данные в файле: "random.txt"');
+  console.log('\nСпасибо, запись прервана, данные в файле: "random.txt"');
   process.exit();
 });
