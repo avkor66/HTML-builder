8c5ba0c6403061db6655bc1948287e984642be1f
Artem Korotkov <avkor66@gmail.com>
2022-05-22 11:59:20 +0500

    feat: completed fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const { readdir, mkdir, rm } = require('fs/promises');
+const fs = require('fs')
+const path = require('path');
 
+
+async function createFolder(newDir, fromDir) {
+  await rm(path.resolve(__dirname, newDir), { recursive: true, force: true }, (err) => {
+    if (err) throw err;
+  })
+  await mkdir(path.resolve(__dirname, newDir), { recursive: true }, (err) => {
+    if (err) throw err;
+  })
+  await copyFiles(path.resolve(__dirname, fromDir), newDir)
+}
+
+async function copyFiles(params, newDir) {
+  const filesForCopy = await readdir(params);
+  filesForCopy.forEach(fl => {
+    let readStream = fs.createReadStream(path.resolve(params, fl));
+    let writeStream = fs.createWriteStream(path.resolve(__dirname, newDir, fl));
+    let temp = ''
+    readStream.on('data', (chunk) => {
+      temp += chunk.toString();
+    })
+    readStream.on('end', () => {
+      writeStream.write(temp)
+    })
+  })
+}
+
+createFolder('files-copy', 'files')
